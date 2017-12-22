b57a79993930d85a6b43120d3c2265f7d0a13690
emrinalgr <rinaldi.gregory@gmail.com>
2017-12-22 16:47:49 +0100

    fix liquibase

--- service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/dto/QueryParameterDto.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/dto/QueryParameterDto.java
@@ -18,6 +18,7 @@ import static eu.europa.ec.fisheries.uvms.commons.date.DateUtils.START_OF_TIME;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import eu.europa.ec.fisheries.uvms.subscription.service.domain.AccessibilityType;
 import eu.europa.ec.fisheries.uvms.subscription.service.domain.MessageType;
 import eu.europa.ec.fisheries.uvms.subscription.service.domain.SubscriptionType;
 import lombok.AllArgsConstructor;
@@ -39,6 +40,7 @@ public class QueryParameterDto {
     private MessageType messageType;
     private SubscriptionType subscriptionType;
     private String description;
+    private AccessibilityType accessibility;
 
    @JsonFormat(shape = STRING, pattern = DATE_TIME_UI_FORMAT)
    private Date startDate = START_OF_TIME.toDate();
