db9e89a1ce1321ffca5629c3b2e39be668ea62ec
gmanifavas <george.manifavas@sword-group.com>
2020-04-16 16:25:50 +0300

    [UNIONVMS-4473] Add email configuration

--- service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/bean/SubscriptionService.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/bean/SubscriptionService.java
@@ -1,32 +1,41 @@
+/*
+ Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2016.
+
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
 package eu.europa.ec.fisheries.uvms.subscription.service.bean;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import eu.europa.ec.fisheries.uvms.subscription.service.domain.EmailBodyEntity;
+import eu.europa.ec.fisheries.uvms.subscription.service.domain.SubscriptionEntity;
 import eu.europa.ec.fisheries.uvms.subscription.service.domain.search.SubscriptionListQuery;
 import eu.europa.ec.fisheries.uvms.subscription.service.dto.SubscriptionDto;
 import eu.europa.ec.fisheries.uvms.subscription.service.dto.list.SubscriptionListResponseDto;
 import eu.europa.ec.fisheries.wsdl.subscription.module.SubscriptionDataQuery;
 import eu.europa.ec.fisheries.wsdl.subscription.module.SubscriptionPermissionResponse;
-import lombok.SneakyThrows;
 
 public interface SubscriptionService {
 	SubscriptionPermissionResponse hasActiveSubscriptions(SubscriptionDataQuery query);
 
-	@SneakyThrows
 	SubscriptionListResponseDto listSubscriptions(@Valid @NotNull SubscriptionListQuery queryParams, String scopeName, String roleName);
 
-	@SneakyThrows
 	SubscriptionDto findById(@NotNull Long id);
 
-	@SneakyThrows
 	SubscriptionDto create(@Valid @NotNull SubscriptionDto subscription);
 
-	@SneakyThrows
 	SubscriptionDto update(@Valid @NotNull SubscriptionDto subscription);
 
-	@SneakyThrows
 	void delete(@NotNull Long id);
+
+	EmailBodyEntity createEmailBody(SubscriptionEntity entity, String body);
+
+	EmailBodyEntity updateEmailBody(SubscriptionEntity entity, String body);
 
 	Boolean checkNameAvailability(@NotNull String name, Long id);
 }
