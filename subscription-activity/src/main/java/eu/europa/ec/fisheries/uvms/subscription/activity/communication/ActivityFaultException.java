1f33a778940cbbed3e99879020a19293cae8b11e
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2020-07-11 12:25:56 +0300

    Refactoring: Better exception handling for the JmsActivityClient, can deal with ActivityFault

---
+++ subscription-activity/src/main/java/eu/europa/ec/fisheries/uvms/subscription/activity/communication/ActivityFaultException.java
@@ -0,0 +1,28 @@
+package eu.europa.ec.fisheries.uvms.subscription.activity.communication;
+
+import eu.europa.fisheries.uvms.subscription.model.exceptions.ApplicationException;
+
+/**
+ * Fault returned from the activity module.
+ */
+public class ActivityFaultException extends ApplicationException {
+	private Integer code;
+	private String fault;
+
+	public ActivityFaultException() {
+	}
+
+	public ActivityFaultException(int code, String fault, String message) {
+		super(message);
+		this.code = code;
+		this.fault = fault;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getFault() {
+		return fault;
+	}
+}
