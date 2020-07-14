1ff8971bd9a553334a553f339f7bbf877ca5f83d
gmanifavas <george.manifavas@sword-group.com>
2020-07-14 19:55:23 +0300

    [UNIONVMS-4485] Trigger FA Report forwarding from incoming position.

--- subscription-activity/src/main/java/eu/europa/ec/fisheries/uvms/subscription/activity/execution/FaReportAndEmailSubscriptionExecutor.java
+++ subscription-activity/src/main/java/eu/europa/ec/fisheries/uvms/subscription/activity/execution/FaReportAndEmailSubscriptionExecutor.java
@@ -1,16 +1,19 @@
 package eu.europa.ec.fisheries.uvms.subscription.activity.execution;
 
+import static eu.europa.ec.fisheries.uvms.subscription.activity.ActivityConstants.KEY_REPORT_ID_PREFIX;
 import static eu.europa.ec.fisheries.uvms.subscription.activity.ActivityConstants.KEY_TRIP_ID_PREFIX;
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
-import static eu.europa.ec.fisheries.uvms.subscription.activity.ActivityConstants.KEY_REPORT_ID_PREFIX;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.time.ZonedDateTime;
 import java.util.Base64;
 import java.util.Comparator;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -60,14 +63,16 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 	private AssetSender assetSender;
 	private SubscriptionExecutionService subscriptionExecutionService;
 	private EmailService emailService;
+	private DatatypeFactory datatypeFactory;
 
 	@Inject
-	public FaReportAndEmailSubscriptionExecutor(ActivitySender activitySender, UsmSender usmSender, AssetSender assetSender, SubscriptionExecutionService subscriptionExecutionService, EmailService emailService) {
+	public FaReportAndEmailSubscriptionExecutor(ActivitySender activitySender, UsmSender usmSender, AssetSender assetSender, SubscriptionExecutionService subscriptionExecutionService, EmailService emailService, DatatypeFactory datatypeFactory) {
 		this.activitySender = activitySender;
 		this.usmSender = usmSender;
 		this.assetSender = assetSender;
 		this.subscriptionExecutionService = subscriptionExecutionService;
 		this.emailService = emailService;
+		this.datatypeFactory = datatypeFactory;
 	}
 
 	/**
@@ -83,12 +88,17 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 		TriggeredSubscriptionEntity triggeredSubscription = execution.getTriggeredSubscription();
 		SubscriptionEntity subscription = triggeredSubscription.getSubscription();
 		if (TRUE.equals(subscription.getOutput().getHasEmail()) || subscription.getOutput().getMessageType() == OutgoingMessageType.FA_REPORT) {
+			ReceiverAndDataflow receiverAndDataflow = Optional.ofNullable(subscription.getOutput().getSubscriber())
+					.filter(s -> subscription.getOutput().getMessageType() == OutgoingMessageType.FA_REPORT)
+					.filter(subscriber -> subscriber.getEndpointId() != null && subscriber.getChannelId() != null)
+					.map(subscriber -> usmSender.findReceiverAndDataflow(subscriber.getEndpointId(), subscriber.getChannelId()))
+					.orElseGet(ReceiverAndDataflow::new);
 			switch (subscription.getExecution().getTriggerType()) {
 				case INC_FA_REPORT:
-					fromReportTrigger(subscription, triggeredSubscription, execution);
+					fromReportTrigger(subscription, triggeredSubscription, execution, receiverAndDataflow);
 					break;
 				case INC_POSITION:
-					log.warn("cannot handle subscriptions triggered by INC_POSITION _yet_");
+					fromPositionTrigger(subscription, triggeredSubscription, execution, receiverAndDataflow);
 					break;
 				default:
 					log.warn("cannot handle subscriptions triggered by {}", subscription.getExecution().getTriggerType());
@@ -126,12 +136,7 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 		return new EmailAttachment(attachmentResponseObject.getTripId(), attachmentResponseObject.getType().value(), attachmentResponseObject.getContent());
 	}
 
-	private void fromReportTrigger(SubscriptionEntity subscription, TriggeredSubscriptionEntity triggeredSubscription, SubscriptionExecutionEntity execution) {
-		ReceiverAndDataflow receiverAndDataflow = Optional.ofNullable(subscription.getOutput().getSubscriber())
-				.filter(s -> subscription.getOutput().getMessageType() == OutgoingMessageType.FA_REPORT)
-				.filter(subscriber -> subscriber.getEndpointId() != null && subscriber.getChannelId() != null)
-				.map(subscriber -> usmSender.findReceiverAndDataflow(subscriber.getEndpointId(), subscriber.getChannelId()))
-				.orElseGet(ReceiverAndDataflow::new);
+	private void fromReportTrigger(SubscriptionEntity subscription, TriggeredSubscriptionEntity triggeredSubscription, SubscriptionExecutionEntity execution, ReceiverAndDataflow receiverAndDataflow) {
 		Map<String, String> dataMap = toDataMap(triggeredSubscription);
 		if (TRUE.equals(subscription.getOutput().getLogbook())) {
 			activitySender.forwardFaReportWithLogbook(
@@ -162,6 +167,25 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 		}
 	}
 
+	private void fromPositionTrigger(SubscriptionEntity subscription, TriggeredSubscriptionEntity triggeredSubscription, SubscriptionExecutionEntity execution, ReceiverAndDataflow receiverAndDataflow) {
+		Map<String, String> dataMap = toDataMap(triggeredSubscription);
+		activitySender.forwardFaReportFromPosition(
+				execution.getId(),
+				TRUE.equals(subscription.getOutput().getGenerateNewReportId()),
+				receiverAndDataflow.getReceiver(),
+				receiverAndDataflow.getDataflow(),
+				TRUE.equals(subscription.getOutput().getConsolidated()),
+				TRUE.equals(subscription.getOutput().getLogbook()),
+				calculateStartDate(triggeredSubscription.getId(), dataMap),
+				calculateEndDate(triggeredSubscription.getId(), dataMap),
+				extractAssetGuid(TRUE.equals(subscription.getOutput().getHasEmail()), triggeredSubscription.getId(), dataMap),
+				extractAssetHistGuid(triggeredSubscription.getId(), dataMap),
+				TRUE.equals(subscription.getOutput().getHasEmail()),
+				Optional.ofNullable(subscription.getOutput().getEmailConfiguration()).map(SubscriptionEmailConfiguration::getIsPdf).orElse(FALSE),
+				Optional.ofNullable(subscription.getOutput().getEmailConfiguration()).map(SubscriptionEmailConfiguration::getIsXml).orElse(FALSE)
+		);
+	}
+
 	private String extractAssetGuid(boolean hasEmail, Long triggeredSubscriptionId, Map<String, String> dataMap) {
 		if (!hasEmail) {
 			return null;
@@ -171,6 +195,12 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 		VesselIdentifiersHolder vesselIdentifiers = assetSender.findVesselIdentifiers(connectId);
 		Objects.requireNonNull(vesselIdentifiers.getAssetGuid(), "asset GUID null for connectId " + connectId + " of " + triggeredSubscriptionId);
 		return vesselIdentifiers.getAssetGuid();
+	}
+
+	private String extractAssetHistGuid(Long triggeredSubscriptionId, Map<String, String> dataMap) {
+		String connectId = dataMap.get(TriggeredSubscriptionDataUtil.KEY_CONNECT_ID);
+		Objects.requireNonNull(connectId, "connectId not found in data of " + triggeredSubscriptionId);
+		return connectId;
 	}
 
 	private List<String> extractReportIds(Map<String, String> dataMap) {
@@ -187,5 +217,25 @@ public class FaReportAndEmailSubscriptionExecutor implements SubscriptionExecuto
 				.sorted(Comparator.comparingInt(e -> Integer.parseInt(e.getKey().substring(KEY_TRIP_ID_PREFIX.length()))))
 				.map(Map.Entry::getValue)
 				.collect(Collectors.toList());
+	}
+
+	private XMLGregorianCalendar calculateStartDate(Long triggeredSubscriptionId, Map<String, String> dataMap) {
+		String occurrence = extractOccurrenceDate(triggeredSubscriptionId, dataMap);
+		ZonedDateTime zdt = ZonedDateTime.parse(occurrence);
+		ZonedDateTime startOfDay = zdt.toLocalDate().atStartOfDay(zdt.getZone());
+		return datatypeFactory.newXMLGregorianCalendar(GregorianCalendar.from(startOfDay));
+	}
+
+	private XMLGregorianCalendar calculateEndDate(Long triggeredSubscriptionId, Map<String, String> dataMap) {
+		String occurrence = extractOccurrenceDate(triggeredSubscriptionId, dataMap);
+		ZonedDateTime zdt = ZonedDateTime.parse(occurrence);
+		ZonedDateTime startOfNextDay = zdt.toLocalDate().atStartOfDay(zdt.getZone()).plusDays(1);
+		return datatypeFactory.newXMLGregorianCalendar(GregorianCalendar.from(startOfNextDay));
+	}
+
+	private String extractOccurrenceDate(Long triggeredSubscriptionId, Map<String, String> dataMap) {
+		String occurrence = dataMap.get(TriggeredSubscriptionDataUtil.KEY_OCCURRENCE);
+		Objects.requireNonNull(occurrence, "occurence not found in data of " + triggeredSubscriptionId);
+		return occurrence;
 	}
 }
