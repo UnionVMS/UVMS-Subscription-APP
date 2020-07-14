95761e8d965c0c59e69abc41616c232483a89dac
lsotiriadis <leontion.sotiriadis@sword-group.com>
2020-07-14 23:30:42 +0300

    [UNIONVMS-4709] Alert output
    Signed-off-by: lsotiriadis <leontion.sotiriadis@sword-group.com>

---
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/rules/communication/RulesSender.java
@@ -0,0 +1,31 @@
+/*
+ Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries @ European Union, 2015-2016.
+
+ This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ */
+package eu.europa.ec.fisheries.uvms.subscription.rules.communication;
+
+import java.util.Date;
+import java.util.Map;
+
+import eu.europa.ec.fisheries.wsdl.asset.types.VesselIdentifiersHolder;
+
+/**
+ * Service for sending messages to Rules module.
+ */
+public interface RulesSender {
+
+	/**
+	 * Send message to Rule to create an alarm report.
+	 *
+	 * @param subscriptionName  The name of the {@code SubscriptionEntity}
+	 * @param openDate The rule open date
+	 * @param vesselIdentifiers The vessel ids, including the asset guid
+	 * @param dataMap The dataMap of {@code TriggeredSubscriptionEntity}, needed to extract required information
+	 */
+	void createAlarmReportAsync(String subscriptionName, Date openDate, VesselIdentifiersHolder vesselIdentifiers, Map<String,String> dataMap);
+}
