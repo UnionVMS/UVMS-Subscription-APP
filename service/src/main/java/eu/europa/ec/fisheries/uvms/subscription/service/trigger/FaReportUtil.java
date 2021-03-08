6ee60cb198461debc1359f4bb70e4bf842c0f822
gmanifavas <george.manifavas@sword-group.com>
2021-03-08 19:10:13 +0200

    [UNIONVMS-5056] Check both occurrenceDate/specifiedDelimitedPeriod for incoming reports.

--- service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/trigger/FaReportUtil.java
+++ service/src/main/java/eu/europa/ec/fisheries/uvms/subscription/service/trigger/FaReportUtil.java
@@ -80,8 +80,7 @@ public class FaReportUtil implements FaReportUtility {
     }
 
     private void enrichWithUserAreas(ReportToSubscription reportToSubscription) {
-        List<XMLGregorianCalendar> dates = reportToSubscription.getFishingActivities().stream().map(fa ->
-                fa.getOccurrenceDateTime().getDateTime()).collect(Collectors.toList());
+        List<XMLGregorianCalendar> dates = reportToSubscription.getFishingActivities().stream().map(this::getFishingActivityDateTime).collect(Collectors.toList());
         List<String> wktList = reportToSubscription.getActivitiesWktLists();
         List<SubscriptionMovementMetaDataAreaTypeResponseElement> responseList = subscriptionSpatialService.getFishingActivitiesUserAreasEnrichmentByWkt(indexed(wktList, dates::get));
         int index = 0;
@@ -92,6 +91,10 @@ public class FaReportUtil implements FaReportUtility {
         }
     }
 
+    private XMLGregorianCalendar getFishingActivityDateTime(FishingActivity fishingActivityEntity) {
+        return fishingActivityEntity.getOccurrenceDateTime() != null ? fishingActivityEntity.getOccurrenceDateTime().getDateTime() : getFirstDateFromDelimitedPeriods(fishingActivityEntity.getSpecifiedDelimitedPeriods()).getDateTime();
+    }
+    
     private static <K, V> Map<K, V> indexed(List<K> keys, Function<Integer, V> fn) {
         return IntStream.range(0, keys.size())
                 .boxed()
